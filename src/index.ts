458666c7adb7b58cbc0ee9590a6211c8006de963
maximt23-chat <maxtrubitsyn2@mail.ru>
2025-08-06 16:23:03 +0300

    Теперь webpack понимает typescript засчет ts-loader, настроил его и реализовал tsconfig

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { calc } from "./script";
+console.log("hello world");
+console.log(calc(1, 2));
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,34 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
-
+const webpack = require("webpack");
 module.exports = (env) => {
   return {
     mode: env.mode ?? "development",
-    entry: path.resolve(__dirname, "src", "index.js"),
+    entry: path.resolve(__dirname, "src", "index.ts"),
     output: {
       path: path.resolve(__dirname, "dist"),
       filename: "[name].[fullhash].js",
       clean: true,
     },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader", //*Используем в качетсве обработчика
+          exclude: /node_modules/, //*Это мы не обрабатываем
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"], //*Теперь импорты можно писать без расширения (import ts from 'ts[.tsx] - не нужно писать tsx')
+    },
     plugins: [
       new HTMLWebpackPlugin({
         template: path.resolve(__dirname, "public", "index.html"),
+      }),
+      //?Замедляет сборку, в проде не используем
+      new webpack.ProgressPlugin({
+        activeModules: true,
       }),
     ],
   };
