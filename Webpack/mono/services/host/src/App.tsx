3875f392989a6b9d85ad49834e99087b48263c7e
maximt23-chat <maxtrubitsyn2@mail.ru>
2025-08-08 19:51:23 +0300

    Подготовил монорепозиторий
    *Создал новую папку, где будет храниться версия сайта монорепозитория
    *Подготовил структуру микрофрнотов
    *Подтянул все правила в package.json, установил в корневой node_modules все необходимые плагины
    *Подправил package.json в host (главный соединяющий микрофронтенд)
    *Пока что подключил к каждому файлу свой tsconfig, в будущем исправлю

---
+++ Webpack/mono/services/host/src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import * as styles from "./App.module.scss";
+import { Link } from "react-router-dom";
+import Alien from "@/assets/alien.svg";
+
+function fun1() {
+  fun2();
+}
+function fun2() {
+  throw new Error();
+}
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount((prev) => prev + 1);
+  let isPlatform;
+  if (__PLATFORM__ == "desktop") {
+    isPlatform = <div>this is some popup</div>;
+  }
+  if (__PLATFORM__ == "mobile") {
+    isPlatform = <div>this is some admin panel</div>;
+  }
+  return (
+    <div className={styles.wrapper} data-testid={"App.DataTestId"}>
+      <div className={styles.links}>
+        <Link
+          className={styles.link}
+          to="/shop"
+          data-testid={"Platform.DataTestId"}
+        >
+          Shop
+        </Link>
+        <Link className={styles.link} to="/admin">
+          Admin
+        </Link>
+      </div>
+      <h2 className={styles.subtitle}>Platform: {isPlatform}</h2>
+      <h1 className={styles.title}>Welcome to my Webpack bundle</h1>
+      <Alien className={styles.img} />
+      <button onClick={increment} className={styles.count}>
+        {count}
+      </button>
+      <button className={styles.btn} onClick={() => fun1()}>
+        Button
+      </button>
+    </div>
+  );
+}
---
+++ Webpack/mono/services/host/webpack.config.ts
@@ -0,0 +1,21 @@
+import path from "path";
+import webpack from "webpack";
+import { buildWebpack, CustomPath, EnvMode } from "@packages/build-config";
+
+module.exports = (env: EnvMode) => {
+  const paths: CustomPath = {
+    output: path.resolve(__dirname, "build"),
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    html: path.resolve(__dirname, "public", "index.html"),
+    src: path.resolve(__dirname, "src"),
+    public: path.resolve(__dirname, "public"),
+  };
+  const config: webpack.Configuration = buildWebpack({
+    port: env.port ?? 3000,
+    mode: env.mode ?? "development",
+    paths,
+    analyzer: env.analyzer,
+    platform: env.platform ?? "desktop",
+  });
+  return config;
+};
